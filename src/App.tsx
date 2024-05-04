57ced544bdd4d27a2e50387017cef72f08095e7c
yusufgns <gunesyusuf838@gmail.com>
2024-05-04 16:39:48 +0300

    feat: shadcn & custom privileges component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
 import { PRIVILEGES } from './constants/privileges'
 import './App.css'
 import { CustomPrivilegesComponent } from './components/privileges/custom'
+import { Button } from './components/ui/button'
+import { PrivilegesController } from './utils/privileges'
 
 function App() {
   return (
-    <>
+    <div className='flex items-center flex-col gap-4'>
       <CustomPrivilegesComponent
         userPrivileges={[PRIVILEGES.PAID]}
-        componentPrivileges={[PRIVILEGES.PAID]}
+        componentPrivileges={[PRIVILEGES.FREE]}
+        active="tooltip"
+      >
+        <Button disabled={!PrivilegesController([PRIVILEGES.FREE], [PRIVILEGES.PAID])}>Hover Me</Button>
+      </CustomPrivilegesComponent>
+
+      <CustomPrivilegesComponent
+        userPrivileges={[PRIVILEGES.PAID]}
+        componentPrivileges={[PRIVILEGES.FREE]}
         active="button"
       >
-        TEST PRIVILEGES
+        <Button>Click me</Button>
       </CustomPrivilegesComponent>
-    </>
+
+      <CustomPrivilegesComponent
+        userPrivileges={[PRIVILEGES.PAID]}
+        componentPrivileges={[PRIVILEGES.FREE]}
+        active="not_privileges"
+      >
+        <Button disabled={!PrivilegesController([PRIVILEGES.FREE], [PRIVILEGES.PAID])}>Click me</Button>
+      </CustomPrivilegesComponent>
+
+      <CustomPrivilegesComponent
+        userPrivileges={[PRIVILEGES.PAID]}
+        componentPrivileges={[PRIVILEGES.FREE]}
+        active="not_privileges"
+        not_privileges='pages'
+      >
+        <Button disabled={!PrivilegesController([PRIVILEGES.FREE], [PRIVILEGES.PAID])}>Click me</Button>
+      </CustomPrivilegesComponent>
+    </div>
   )
 }
 
--- src/components/privileges/custom.tsx
+++ src/components/privileges/custom.tsx
@@ -1,19 +1,16 @@
 import { PrivilegesContainer } from "./container";
+import { Button } from "../ui/button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
+import WarningSVG from "@/components/icons/warning";
+import { moderator } from "@/constants/privileges";
 
-export type Privilege = {
-    [key: string]: string;
-}
-
-export const moderator: Readonly<Privilege> = Object.freeze({
-    ADMIN: 'admin',
-});
-
-export const CustomPrivilegesComponent = ({ children, userPrivileges, componentPrivileges, active }: {
+export const CustomPrivilegesComponent = ({ children, userPrivileges, componentPrivileges, active, not_privileges = "component" }: {
     children: React.ReactNode;
     userPrivileges: string[];
     // moderator: string[];
     componentPrivileges: string | string[];
     active: "button" | "tooltip" | "not_privileges";
+    not_privileges?: "pages" | "component";
 }) => {
     return (
         <PrivilegesContainer
@@ -21,11 +18,43 @@ export const CustomPrivilegesComponent = ({ children, userPrivileges, componentP
             userPrivileges={userPrivileges}
             componentPrivileges={componentPrivileges}
             active={active}
-            button_container={<button>Test</button>}
-            not_privileges_container={<div>Not enough privileges</div>}
-            tooltip_container={<div>Tooltip</div>}
-        >
-            {children}
-        </PrivilegesContainer>
+            button_container={
+                <Button disabled className="select-none">
+                    You are not privileges for this
+                </Button>
+            }
+            not_privileges_container={
+                not_privileges === "component"
+                    ? (
+                        <div className="p-2 rounded-md border text-orange-600 border-orange-500 select-none bg-orange-50">
+                            <WarningSVG className="w-5 h-5 mr-2 inline" />
+                            You are not privileges for component
+                        </div>
+                    )
+                    : (
+                        <div className="p-2 select-none space-y-3">
+                            <WarningSVG className="w-16 h-16 mr-2 inline" />
+                            <p>You are not privileges for pages</p>
+                            <Button>
+                                Go to home
+                            </Button>
+                        </div>
+                    )
+            }
+            tooltip_container={
+                <TooltipProvider
+                    delayDuration={0}
+                >
+                    <Tooltip>
+                        <TooltipTrigger className="p-0 h-fit w-fit select-none">
+                            {children}
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            You are not privileges for this
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+            }
+        />
     )
 }
